310ce13d5d212d23a29f8f1e3adf9026c15a7178
albertribeirov <albertribeiro@live.com>
2018-10-24 23:30:37 -0300

    Correcao intersecao

--- java/br/com/sislancamento/Intersecao.java
+++ java/br/com/sislancamento/Intersecao.java
@@ -2,30 +2,48 @@ package br.com.sislancamento;
 
 public class Intersecao implements  {
 
-	private int valor_1;
-	private int valor_2;
+	private int val_1;
+	private int val_2;
+	private int val_3;
+	private int val_4;
 	
 	public int getvalor1() {
-		return valor_1;
+		return val_1;
+	}
+	
+	public void setvalor1(int val_1) {
+		this.val_1 = val_1;
 	}
 	
 	public int getvalor2() {
-		return valor_2;
-	}
-
-	public void setvalor1(int valor_1) {
-		this.valor_1 = valor_1;
+		return val_2;
 	}
 	
-	public void setvalor2(int valor_2) {
-		this.valor_2 = valor_2;
+	public void setvalor2(int val_2) {
+		this.val_2 = val_2;
+	}
+	
+	public int getvalor3() {
+		return val_3;
+	}
+	
+	public void setvalor3(int val_3) {
+		this.val_3 = val_3;
+	}
+	
+	public int getvalor4() {
+		return val_4;
+	}
+	
+	public void setvalor4(int val_4) {
+		this.val_4 = val_4;
 	}
 	
 	public String verificarIntersecao(String valor1, String valor2, String valor3, String valor4) {
-		Integer val_1 = Integer.parseInt(valor1);
-		Integer val_2 = Integer.parseInt(valor2);
-		Integer val_3 = Integer.parseInt(valor3);
-		Integer val_4 = Integer.parseInt(valor4);
+		val_1 = Integer.parseInt(valor1);
+		val_2 = Integer.parseInt(valor2);
+		val_3 = Integer.parseInt(valor3);
+		val_4 = Integer.parseInt(valor4);
 
 		if (val_3 > val_1 && val_3 < val_2) {
 			FacesContext.getCurrentInstance().addMessage("form:button", new FacesMessage("Existe interseção entre as faixas 1 e 2."));
